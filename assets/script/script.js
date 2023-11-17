154e0dc8109c24f1b17d5f3bc0fac5d52c6a5ec1
RETR0EDIT <nolandme2003@gmail.com>
2023-11-17 12:03:38 +0100

    first

---
+++ assets/script/script.js
@@ -0,0 +1,37 @@
+(function(){
+
+    const carousel = document.querySelector('.carousel');
+    const prev = document.querySelector('#prev');
+    const next = document.querySelector('#next');
+        
+    let faceCount = 3;
+
+    let stepCount = 30;
+
+    let count = 0;
+
+    prev.addEventListener('click', () => {
+        let stepForward = count += stepCount;
+        nextStep(stepForward);
+    });
+
+    next.addEventListener('click', () => {
+        let stepReward = count -= stepCount;
+        nextStep(stepReward);
+    });
+
+    function nextStep(x) {
+        if (x < -60) {
+            x = 0;
+            count = 0;
+        } else if (x >= 0 && x !== 0) {
+            x = -60;
+            count = -60
+        }
+
+        carousel.style.transform = `rotateY(${x}deg)`;
+    }
+
+
+    
+}());
---
+++ public/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<header>
+    <nav>
+
+    </nav>
+</header>
+<body>
+<section class="carousel_scene">
+
+    <section class="carousel">
+
+        <div class="face f_01"> </div>
+        <div class="face backface back_01"></div>
+
+        <div class="face f_02"> </div>
+        <div class="face backface back_02"></div>
+
+        <div class="face f_03"> </div>
+        <div class="face backface back_03"></div>
+
+    </section>
+
+</section>
+
+
+
+
+<section class="button_wrap">
+
+    <button class="button" id="prev">prev</button>
+    <button class="button" id="next">next</button>
+
+    
+</section>
+<script src="../assets/script/script.js"></script>
+</body>
+</html>
