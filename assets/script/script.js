869d3bad8004a04558d725fd31bccc6d3431a60b
RETR0EDIT <nolandme2003@gmail.com>
2023-11-17 23:25:50 +0100

    update carou div au centre

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,21 +1,20 @@
-(function(){
-
+(function() {
     const carousel = document.querySelector('.carousel');
     const prev = document.querySelector('#prev');
     const next = document.querySelector('#next');
-        
+    
     let faceCount = 3;
-
     let stepCount = 30;
+    let count = -30;
 
-    let count = 0;
+
+    nextStep(count);
 
     prev.addEventListener('click', () => {
         let stepForward = count += stepCount;
         nextStep(stepForward);
     });
 
-    
     next.addEventListener('click', () => {
         let stepReward = count -= stepCount;
         nextStep(stepReward);
@@ -25,14 +24,13 @@
         if (x < -60) {
             x = 0;
             count = 0;
+
         } else if (x >= 0 && x !== 0) {
             x = -60;
-            count = -60
+            count = -60;
+
         }
 
         carousel.style.transform = `rotateY(${x}deg)`;
     }
-
-
-    
 }());
