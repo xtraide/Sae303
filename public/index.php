faf6d85dd3f939186c0e1bbed479818a471a2c09
RETR0EDIT <nolandme2003@gmail.com>
2023-11-19 15:48:01 +0100

    responsive

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../assets/style/style.css">
+    <link rel="stylesheet" href="../assets/style/responsive.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Aéro Club</title>
 </head>
 <header>
     <nav>
@@ -17,7 +18,7 @@
 
 <body>
     <section class="carousel_scene position-absolute top-20 start-50 translate-middle-x"
-        style="width: 200px; height: 110px; perspective: 640px;">
+        style="width: 180px; height: 110px; perspective: 640px;">
 
         <section class="carousel position-relative w-100 h-100"
             style="transform-style: preserve-3d; transition: transform 0.4s;">
@@ -43,7 +44,6 @@
         <button class="button cursor-pointer width-6rem height-6rem m-2 fs-3" id="prev">prev</button>
         <button class="button cursor-pointer width-6rem height-6rem m-2 fs-3" id="next">next</button>
     </section>
-
     <div class="card">
         <h1 id="h1-card">Météo à Vesoul</h1>
         <div class="weather-icon"></div>
