638eee653c7ca482a43ddc87f0b444f5e7816999
RETR0EDIT <86560394+RETR0EDIT@users.noreply.github.com>
2023-11-19 15:18:40 +0100

    Update config.php

--- prive/config.php
+++ prive/config.php
@@ -1,6 +1,6 @@
 <?php
 
 $config = [
-    'api_key' => '84b8fdb1518d9300c156b409f25d1465'
+    'api_key' => '84b8fdb1518d9300c156b409f25d1465' /* nico si tu passe par la faut que je vois avc toi si ya pas un moyen de crypter la clé api en attendant pour la remove j'ai fait ca et tu peux modif stv*/
 ];
 ?>
