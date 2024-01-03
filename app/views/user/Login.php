806ffb9d3e459ff76b693e2573af834bdd076460
nicolas <xxtraide@gmail.com>
2024-01-03 12:49:02 +0100

    30/12

--- app/views/user/Login.php
+++ app/views/user/Login.php
@@ -1,25 +1,14 @@
-<?php
-
-if ($error) {
-?>
-    <div class="alert alert-danger">
-        Identifiant ou mot de passe incorrect
-    </div>
-<?php
-}
-
-
-?>
 <div class="w-100 vh-100 background d-flex align-items-center">
     <form action="#" method="post" class="w-50 m-auto ">
-        <div class="form-group w-100 m-auto">
+        <h1 class="text-center">Login</h1>
+        <div class="form-group w-100 m-auto label-log">
             <?= $form->input('email', "Adresse email", ['class' => 'form-control w-100', 'id' => 'email']); ?>
         </div>
-        <div class="form-group w-100 m-auto">
+        <div class="form-group w-100 m-auto label-log">
             <?= $form->input('password', "Mot de passe", ['type' => 'password', 'class' => 'form-control w-100 back_input']); ?>
         </div>
-        <div class="form-group">
-            <?= $form->submit(['class' => 'btn btn-primary d-flex align-items-center', 'value' => 'Se connecter']); ?>
+        <div class="form-group m-auto">
+            <?= $form->submit(['class' => 'btn btn-primary d-flex align-items-center m-auto ', 'value' => 'Se connecter']); ?>
         </div>
     </form>
 </div>
