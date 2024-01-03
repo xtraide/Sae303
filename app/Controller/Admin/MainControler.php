7710a10cf45a8acbdab8a9d2c3676bb1ee873a28
nicolas <xxtraide@gmail.com>
2024-01-03 13:25:06 +0100

    controller

---
+++ app/Controller/Admin/MainControler.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+class MainController extends AppController
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->loadModel('Avion');
+    }
+    public function index()
+    {
+        $avions = $this->Avion->all();
+        $this->render('main.index', compact('avions'));
+    }
+}
