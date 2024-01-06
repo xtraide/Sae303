b2305f88216948cc66a5bbac08828b5beccc3ad5
RETR0EDIT <nolandme2003@gmail.com>
2024-01-06 21:35:20 +0100

    html

--- app/views/Main/Index.php
+++ app/views/Main/Index.php
@@ -372,30 +372,6 @@
                     </div>
                 </div>
             </div>
-            <!--formulaire de contact   -->
-            <div class="collapse" id="contactForm">
-                <div class="card-body">
-                    <form>
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Nom</label>
-                            <input type="text" class="form-control" id="name" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="subject" class="form-label">Sujet</label>
-                            <input type="text" class="form-control" id="subject" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="message" class="form-label">Message</label>
-                            <textarea class="form-control " id="message" rows="3" required></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Envoyer</button>
-                    </form>
-                </div>
-            </div>
         </div>
     </div>
 </section>
