cda91cbb8ff1336f251e102d08c7645558f013ac
Remy <myre@gmail.com>
2024-01-07 15:49:43 +0100

    page aerodrome + css

---
+++ app/views/Main/aerodrome.php
@@ -0,0 +1,127 @@
+<div class="aerodrome container-fluid text-center">
+    <div class="row">
+      <div class="col-md-12  mt-5 pt-5">
+        <div class="background  mt-5 pt-5">
+          <h1>L'AERODROME</h1>
+          <img id="avion_aerodrome" src="PlaneHomepage.png" alt="avion" class="mt-5">
+        </div>
+      </div>
+    </div>
+
+    <div class="equipement container text-center">
+      <p class="w-50 mt-5 mb-5 mx-auto">Equipement</p>
+      <div class="d-flex justify-content-evenly">
+        <div><button id="btn1" >INFRASTRUCTURE</button></div>
+        <div><button id="btn2">INSTALLATIONS</button></div>
+        <div><button id="btn3">SERVICES</button></div>
+      </div>
+      <div class="background_equipement border_equipement pt-5 pb-5" class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="mb-3 mt-3 w-100">
+              <p class="d-flex align-items-right mx-auto w-50 p-4"> <img src="https://via.placeholder.com/50"
+                  alt="placeholder" class="img-thumbnail"> Surface totale
+                de
+                45 hectares dégagés</p>
+            </div>
+            <div class="mb-3">
+              <p class="d-flex align-items-right mx-auto w-50 p-4"> <img src="https://via.placeholder.com/50"
+                  alt="placeholder" class="img-thumbnail"> 2 pistes en X de
+                400 et 800 mètres</p>
+            </div>
+            <div class="">
+              <p class="d-flex align-items-right mx-auto w-50 p-4"> <img src="https://via.placeholder.com/50"
+                  alt="placeholder" class="img-thumbnail"> 8 hectares
+                d'hydrosurface</p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div> <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail"></div>
+          </div>
+        </div>
+      </div>
+
+      <div style="display: none;">
+        <div>
+          <p><img src="https://via.placeholder.com/100" alt="placeholder" class="img-thumbnail"> 3 hangars avec
+            3800m2 de surface</p>
+        </div>
+        <div>
+          <p><img src="https://via.placeholder.com/100" alt="placeholder" class="img-thumbnail">Locaux
+            administratifs informatisés</p>
+        </div>
+        <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+      </div>
+      <div style="display: none;">
+        <div>
+          <p><img src="https://via.placeholder.com/100" alt="placeholder" class="img-thumbnail">Atelier d'entretien
+            et de réparation</p>
+        </div>
+        <div>
+          <p><img src="https://via.placeholder.com/100" alt="placeholder" class="img-thumbnail">Capacités d'accueil
+            et de restauration</p>
+        </div>
+        <div>
+          <p><img src="https://via.placeholder.com/100" alt="placeholder" class="img-thumbnail">Connexion WIFI
+            gratuit</p>
+        </div>
+        <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+      </div>
+    </div>
+
+
+    <div class="container text-center">
+      <p class="w-50 mt-5 mx-auto">Gallerie</p>
+      <div class="row mt-5">
+        <div class="col-md-4 ">
+          <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+        </div>
+        <div class="col-md-4">
+          <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+        </div>
+        <div class="col-md-4">
+          <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-12">
+          <img src="https://via.placeholder.com/900" alt="placeholder" class="img-thumbnail">
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-6">
+          <img src="https://via.placeholder.com/450" alt="placeholder" class="img-thumbnail">
+        </div>
+        <div class="col-md-6">
+          <img src="https://via.placeholder.com/450" alt="placeholder" class="img-thumbnail">
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-4">
+          <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+        </div>
+        <div class="col-md-4">
+          <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+        </div>
+        <div class="col-md-4">
+          <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-4">
+          <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+        </div>
+        <div class="col-md-4">
+          <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+        </div>
+        <div class="col-md-4">
+          <img src="https://via.placeholder.com/300" alt="placeholder" class="img-thumbnail">
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-12">
+          <img src="https://via.placeholder.com/900" alt="placeholder" class="img-thumbnail">
+        </div>
+      </div>
+    </div>
+  </div>
