ed33780a62dc9f8a9c4a605222ac5eb2e5fbf08b
nicolas <xxtraide@gmail.com>
2024-01-08 22:46:52 +0100

    admin.main.index

--- app/Controller/Admin/MainController.php
+++ app/Controller/Admin/MainController.php
@@ -9,11 +9,10 @@ class MainController extends \App\Controller\AppController
     public function __construct()
     {
         parent::__construct();
-        $this->Avion = $this->loadModel('Avion');
     }
     public function index()
     {
-        $avions = $this->Avion->all();
-        $this->render('admin.avion.index', compact('avions'));
+
+        $this->render('admin.main.index');
     }
 }
