3b5138683403ae7090723d0f5e68730f3dd7f900
nicolas <xxtraide@gmail.com>
2024-01-08 22:49:54 +0100

    admin.main.index

--- app/views/Admin/Main/Index.php
+++ app/views/Admin/Main/Index.php
@@ -5,8 +5,8 @@
         <h1 class="mb-4">Page de gestion</h1>
         <div class="list-group">
             <a href="?page=admin.user.index" class="list-group-item list-group-item-action">Gerer les Users</a>
-            <a href="?page=admin.avion.index" class="list-group-item list-group-item-action">Gerer les Airplanes</a>
+            <a href="?page=admin.avion.index" class="list-group-item list-group-item-action">Gerer les Avions</a>
             <a href="?page=admin.reservation.index" class="list-group-item list-group-item-action">Gerer les Reservations</a>
-            <a href="?page=admin.pilote.index" class="list-group-item list-group-item-action">Gerer les Pilots</a>
+            <a href="?page=admin.pilote.index" class="list-group-item list-group-item-action">Gerer les Pilotes</a>
         </div>
     </div>
