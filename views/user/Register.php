04e9f8b8fb21fc75d62e5deba4cdc398efa5c27c
RETR0EDIT <nolandme2003@gmail.com>
2024-01-09 10:42:07 +0100

    login

---
+++ views/user/Register.php
@@ -0,0 +1,11 @@
+Bonjour Mme Chantal
+
+Objet : Justification d'absence les 12/12 et 20/12
+
+Madame, Monsieur,
+
+Je me permets de vous adresser ce courriel afin de vous informer et de justifier mes absences au travail les 12 et 20 décembre.
+
+Le 12 décembre dernier, j'ai dû m'absenter de mon lieu de travail en raison de la nécessité d'aller chercher mon frère à la gare de Disney. Cette situation imprévue a exigé une intervention immédiate de ma part, et bien que j'aie tenté d'anticiper au mieux les circonstances, il m'a été impossible de concilier cette obligation familiale avec ma présence au travail.
+
+De plus, le 20 décembre, en me rendant au travail, j'ai été confronté à une crevaison de la roue de ma voiture. Cette situation a engendré un retard significatif dans mon trajet, rendant impossible ma présence à l'heure prévue au bureau.
