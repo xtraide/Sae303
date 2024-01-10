03a5cc7d7333728dfad5ff332b8145fdabc64e26
RETR0EDIT <nolandme2003@gmail.com>
2024-01-11 00:38:24 +0100

    html css boostrap php

--- app/views/user/Register.php
+++ app/views/user/Register.php
@@ -1,25 +1,25 @@
-<div class="w-100" style="padding-top: 100px; height:80vh;">
+<div class="w-100 police" style="padding-top: 100px; height:80vh;">
     <h1 style="font-family: quicksand;
     font-size: 3rem;
     color: black;
     text-align: center;">Inscription</h1>
     <div class="col-md-6 mx-auto">
 
-        <form action="" method="post" class="w-50 m-auto mb-3">
+        <form action="" method="post" class="w-50 m-auto mb-3 police">
             <?php if (!empty($errorMessage)) : ?>
-                <div class="alert alert-danger">
-                    <?= $errorMessage ?>
-                </div>
+            <div class="alert alert-danger">
+                <?= $errorMessage ?>
+            </div>
             <?php endif; ?>
             <div class="">
             </div>
-            <div class="form-group">
+            <div class="form-group police">
                 <?= $form->input('email', "Adresse email", ['class' => 'form-control w-100', 'id' => 'email']); ?>
             </div>
-            <div class="form-group">
+            <div class="form-group police">
                 <?= $form->input('password', "Mot de passe", ['type' => 'password', 'class' => 'form-control w-100 back_input']); ?>
             </div>
-            <div class="form-group">
+            <div class="form-group police">
                 <?= $form->input('password', "Verification de mot de passe", ['type' => 'password', 'class' => 'form-control w-100 back_input']); ?>
             </div>
             <div class="form-group m-auto">
