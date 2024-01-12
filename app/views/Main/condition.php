e6927154e71a3a60caec1d830711a5c73dcb683a
Remy <myre@gmail.com>
2024-01-12 22:19:20 +0100

    coreection de bug

--- app/views/Main/condition.php
+++ app/views/Main/condition.php
@@ -1,11 +1,11 @@
-<section class="text-dark aerodrome">
+<section class="text-white aerodrome">
     <div style="padding-top: 8%;">
         <div class="h1 text-center">Mentions Légales</div>
-        <div class="h2">ÉDITEUR</div>
+        <div class="h2 text-decoration-underline">ÉDITEUR</div>
         <div>Thieblemont Industry<br>
             Pour l’Aerodrome de Frotey </div>
 
-        <div class="h2">RÉALISATION TECHNIQUE & HÉBERGEMENT DU SITE</div>
+        <div class="h2 text-decoration-underline">RÉALISATION TECHNIQUE & HÉBERGEMENT DU SITE</div>
         <div>Ce site a été conçu et réalisé par la société Thieblemont Industry.
 
             Thieblemont Industry<br>
@@ -18,39 +18,39 @@
 
             Il est quotidiennement mis à jour par le webmaster de la Thieblemont Industry.<br><br> </div>
 
-        <div class="h2">ASSISTANCE À MAÎTRISE D’OUVRAGE</div>
+        <div class="h2 text-decoration-underline">ASSISTANCE À MAÎTRISE D’OUVRAGE</div>
         <div>SILAOS
             18 rue de la Paix<br>
             77120 Coulommiers<br>
             www.t-industry.fr – 01 02 03 45 56 <br><br></div>
 
-        <div class="h2">CARTOGRAPHIES</div>
+        <div class="h2 text-decoration-underline">CARTOGRAPHIES</div>
         <div><br>Les informations disponibles dans les cartographies sont fournies à titre indicatif. Elles n’ont pas, elles-mêmes, de valeur juridique particulière et ne sauraient engager en aucun cas la responsabilité de la Thieblemont Industry.
             <br><br>
         </div>
-        <div class="h2">ACTUALITÉ</div>
+        <div class="h2 text-decoration-underline">ACTUALITÉ</div>
         <div><br>Les informations disponibles dans la rubrique « agenda » sont fournies à titre indicatif. Elles n’ont pas, elles-mêmes, de valeur juridique particulière et ne sauraient engager en aucun cas la responsabilité de Thieblemont Industry.
             <br><br>
         </div>
-        <div class="h2">CRÉDITS PHOTOGRAPHIQUES</div>
+        <div class="h2 text-decoration-underline">CRÉDITS PHOTOGRAPHIQUES</div>
         <div><br>Les photos du site ne sont pas libres de droit.<br>
             Patoche de la compta – Mehdi Media – A. Lamoureux – Julien Gazo – Simon Bourclier – Freepik – Akken – Le Crabe Fantôme – Pixabay – RendR<br><br>
         </div>
-        <div class="h2">DROITS D’AUTEURS</div>
+        <div class="h2 text-decoration-underline">DROITS D’AUTEURS</div>
         <div><br>* La structure générale du portail, ainsi que les textes, photos, images et sons composant ce portail sont la propriété de Thieblemont Industry. En application de la loi du 11 mars 1957 (art. 41) et du Code de la propriété intellectuelle du 1er juillet 1992, toute reproduction partielle ou totale à usage collectif, par quelque procédé que ce soit, est strictement interdite sans autorisation de Thieblemont Industry.<br>
 
             * Les logos, visuels et marques présents sur ce site sont la propriété de leur détenteur respectif.<br>
 
             * L’internaute reconnaît que l’utilisation du site de Thieblemont Industry est régie par le droit français.<br><br>
         </div>
-        <div class="h2">LIENS HYPERTEXTES</div>
+        <div class="h2 text-decoration-underline">LIENS HYPERTEXTES</div>
         <div><br>* Les liens hypertextes externes mis en place dans le cadre du présent portail et les contenus des sites de tiers vers lesquels ils pointent ne sauraient engager la responsabilité de Thieblemont Industry.<br>
 
             <br>* Ce portail peut, à son insu, avoir été relié à d’autres sites par le biais de liens hypertextes.<br><br>
 
             La Thieblemont Industry décline toute responsabilité pour les informations présentées sur ces autres sites.<br><br>
         </div>
-        <div class="h2">TRAITEMENT DES DONNÉES PERSONNELLES</div>
+        <div class="h2 text-decoration-underline">TRAITEMENT DES DONNÉES PERSONNELLES</div>
         <div><br>Les données personnelles recueillies sur le site résultent de la communication volontaire d’une adresse de courrier électronique lors du dépôt d’un message électronique. Les courriels ainsi recueillis ne servent qu’à transmettre les éléments d’informations demandés.<br>
 
             <br>Les adresses électroniques collectées ne feront l’objet d’aucune cession, ni d’aucun traitement de la part de Thieblemont Industry sauf en cas d’accord de la part du destinataire de recevoir des informations de l’industrie et/ou de ses partenaires.<br>
